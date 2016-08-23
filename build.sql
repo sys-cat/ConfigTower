d25c02d900d14a08af22b24ad5af64ca4b6d7390
sys_cat <systemcat91@gmail.com>
2016-08-23 16:35:29 +0900

    modify build.sql

--- build.sql
+++ build.sql
@@ -1,18 +1,18 @@
 create table if not exists users(
-    id              int(11),
-    name            varchar(254),
-    mail            varchar(254),   # mail address
-    bio             text,           # biography
-    payment         int(11),        # payment price
-    paymented_at    datetime,
-    updated_at      datetime,
-    created_at      datetime
+    id              int(11)         NOT NULL  AUTO_INCREMENT,
+    name            varchar(254)    NOT NULL,
+    mail            varchar(254)    NOT NULL,                   # mail address
+    bio             text,                                       # biography
+    payment         int(11)         DEFAULT 0,                  # payment price
+    paymented_at    datetime        DEFAULT NULL,
+    updated_at      datetime        NOT NULL,
+    created_at      datetime        NOT NULL
 );
 create table if not exists datas(
-    id              int(11),
-    uid             int(11),        # db.users.id
-    env             tinyint(1),
-    json            varchar(254),   # file path
-    updated_at      datetime,
-    created_at      datetime
+    id              int(11)         NOT NULL AUTO_INCREMENT,
+    uid             int(11)         NOT NULL,                   # db.users.id
+    env             tinyint(1)      NOT NULL,
+    json            varchar(254),                               # file path
+    updated_at      datetime        NOT NULL,
+    created_at      datetime        NOT NULL
 );
